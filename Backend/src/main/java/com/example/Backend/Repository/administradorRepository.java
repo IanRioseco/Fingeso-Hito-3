9391fa3123e3fc6175f71cfe410f5ddba929cbb7
Johan <johan.neira@usach.cl>
2025-06-02 16:14:53 -0400

    Se agregaron los controladores y se avanzaron con algunos

--- Backend/src/main/java/com/example/Backend/Repository/administradorRepository.java
+++ Backend/src/main/java/com/example/Backend/Repository/administradorRepository.java
@@ -3,5 +3,10 @@ package com.example.Backend.Repository;
 import com.example.Backend.Entity.administradorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface administradorRepository extends JpaRepository<administradorEntity, Long> {
+
+    /*Este apartado sirve para hacer consultas mas elavoradas*/
+    Optional<administradorEntity> FindByEmail(String email);
 }
